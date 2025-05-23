10db1bdd87979cee52113b10148e1b6747e25bed
DEDONFX <oyebanjilawrence55@gmail.com>
2025-05-23 10:36:15 +0100

    Initial bot code

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Bot
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarku>
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 TOKEN = "7641749973:AAHlZReWYPR3D55IEzZgyePZaJLC2VBbQ5g"
 
 WELCOME_TEXT = """Welcome to CANA — Culture And New Assets
 
-A new wave of ownership. Mint OG NFTs and join our community redefining Education and culture through Web3.
+A new wave of ownership. Mint OG NFTs and join our community redefinin>
 
 Instructions:
 
@@ -19,15 +19,15 @@ IMAGE_URL = "https://i.imgur.com/jNGP9gh.png"
 
 def start(update: Update, context: CallbackContext) -> None:
     keyboard = [
-        [InlineKeyboardButton("CANA WEBSITE", url="https://canabeings.github.io/cana/")],
-        [InlineKeyboardButton("MINT OG NFTs", url="https://canabeings.github.io/OG-NFT-COLLECTION-/")],
-        [InlineKeyboardButton("Telegram Channel", url="https://t.me/canabeing"),
-         InlineKeyboardButton("Follow X", url="https://twitter.com/CANA_BEINGS")],
-        [InlineKeyboardButton("Join Discord", url="https://discord.gg/69EBmXNt")]
+        [InlineKeyboardButton("CANA WEBSITE", url="https://canabeings.>
+        [InlineKeyboardButton("MINT OG NFTs", url="https://canabeings.>
+        [InlineKeyboardButton("Telegram Channel", url="https://t.me/ca>
+         InlineKeyboardButton("Follow X", url="https://twitter.com/CAN>
+        [InlineKeyboardButton("Join Discord", url="https://discord.gg/rGZtz8NF/>
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-    context.bot.send_photo(chat_id=update.effective_chat.id, photo=IMAGE_URL, caption=WELCOME_TEXT, reply_markup=reply_markup)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=IMA>
 
 def main():
     updater = Updater(TOKEN)
